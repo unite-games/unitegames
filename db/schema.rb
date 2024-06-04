26f6a45f7022d4f06ec7732cb198dc8c34d05f3b
Ricardo Rodrigues <originalrcd@hotmail.com>
2024-06-04 11:47:25 -0300

    feat: add for key games

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_03_183022) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_04_143939) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,8 +18,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_03_183022) do
     t.string "title"
     t.string "category"
     t.float "price"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_games_on_user_id"
   end
 
   create_table "transactions", force: :cascade do |t|
@@ -40,10 +42,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_03_183022) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "games", "users"
   add_foreign_key "transactions", "games"
   add_foreign_key "transactions", "users"
 end
