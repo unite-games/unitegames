827f3bd1b6211aa795a476787839aed4c94cb142
felipe imbimbo <f_imbimbo@hotmail.com>
2024-06-04 13:29:33 -0300

    login form correct and checkout routes created

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,5 +4,17 @@ class TransactionsController < ApplicationController
   end
 
   def create
+    @transaction = Transaction.new(transaction_params)
+    if @transaction.save
+      # deu bom
+      redirect_to restaurant_path(@restaurant)
+    else
+      # deu ruim, não salvou por erro de validação
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def transaction_params
+    params.require(:transaction).permit(:datetime, :game_id, :user_id)
   end
 end
