41e77c7a2ad2a2dc72f95c5c505677248955f5e4
Guilherme Ronchi <guironchimail@gmail.com>
2024-06-07 14:13:17 -0300

    Concerted pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,16 @@
 #   end
 require 'faker'
 
-User.create(name: "José", email: "jose@gmail.com", password: "josesenha").save
-30.times do
+
+Transaction.delete_all
+Game.delete_all
+User.delete_all
+
+User.create(name: "José", email: "jose@gmail.com", password: "1234567").save
+
+10.times do
   # User.create!(name:.name, email: "#{Faker::Creature::Animal.name}@gmail.com", password: Faker::App.name)
-  game = Game.new(user: User.last, title: Faker::Game.title, category: Faker::Game.genre, price: 9.99)
+  game = Game.new(user: User.last, title: Faker::Game.title, category: Faker::Game.genre, price: rand(20..60))
   game.save
 end
 # User.create(name: "José", email: "jose@gmail.com", password: "josesenha")
